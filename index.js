3933e60a64b17a123dff4f3f8883de16f04bf49d
Josh Silverman <jrsilverman71@hotmail.com>
2019-07-31 10:16:12 -0700

    setting up node server and connection to mongoDB

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+app.use(express.json({ extented: false }));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
